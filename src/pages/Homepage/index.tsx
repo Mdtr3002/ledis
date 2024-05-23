cdce78bf2760e78a30335a1e8a3fd67e140cf5ca
minh tran <minhduytranct2017@gmail.com>
2024-05-23 15:17:06 +0700

    feat: Create homepage UI
    Create a static UI for homepage, including:
    - Logo + Title
    - Command logs
    - Command input box

--- src/pages/Homepage/index.tsx
+++ src/pages/Homepage/index.tsx
@@ -1,12 +1,49 @@
+import { useState } from 'react';
+
 import RedisIcon from '../../assets/svg/redis_icon.svg';
 
 const Homepage = () => {
+  const [inputCommand, setInputCommand] = useState<string>('');
+  const [commandList, setCommandList] = useState<string[]>(['>SET key value', 'OK']);
+
+  const handleCommand = () => {
+    if (inputCommand === '') return;
+    if (inputCommand.toLowerCase() === 'clear') {
+      setCommandList([]);
+      setInputCommand('');
+      return;
+    }
+    setCommandList((prev) => [...prev, `>${inputCommand}`, 'OK']);
+    setInputCommand('');
+  };
+
   return (
-    <div className='flex items-center justify-center space-x-2 mt-5'>
-      <img src={RedisIcon} alt='Redis Icon' className='w-20 h-20' />
-      <div className='flex flex-col space-y-1'>
-        <h1 className='text-2xl font-bold text-[#D82C20]'>Ledis</h1>
-        <p>Light weight Redis CLI</p>
+    <div className='my-5 flex flex-col items-center w-full'>
+      <div className='flex items-center justify-center space-x-2'>
+        <img src={RedisIcon} alt='Redis Icon' className='w-20 h-20' />
+        <div className='flex flex-col space-y-1'>
+          <h1 className='text-2xl md:text-3xl xl:text-4xl font-bold text-[#D82C20]'>Ledis</h1>
+          <p>Light weight Redis CLI</p>
+        </div>
+      </div>
+      <div className='mt-10 w-full px-5 md:px-10 lg:px-[80px] xl:px-0 xl:w-[50vw]'>
+        <div className='w-full bg-gray-200 h-[400px] rounded-t-lg p-2 flex flex-col overflow-y-auto custom-scrollbar'>
+          {commandList.map((command, index) => (
+            <p key={index}>{command}</p>
+          ))}
+        </div>
+        <div className='w-full bg-black rounded-b-lg p-2 text-white flex space-x-1'>
+          <p>{'>'}Ledis:</p>
+          <input
+            type='text'
+            value={inputCommand}
+            onChange={(e) => setInputCommand(e.target.value)}
+            onKeyDown={(e) => {
+              if (e.key === 'Enter') handleCommand();
+            }}
+            className='w-full bg-transparent outline-none'
+          />
+        </div>
       </div>
     </div>
   );
