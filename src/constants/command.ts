5566c4f5cab0e4243e5d4ee6ca6db086b77905ef
minh tran <minhduytranct2017@gmail.com>
2024-05-25 17:10:09 +0700

    feat: Adjust UI and add tutorial methods

--- src/constants/command.ts
+++ src/constants/command.ts
@@ -9,3 +9,24 @@ export const DataExpireCommand = ['EXPIRE', 'TTL'];
 export const KeyCommand = ['DEL', 'KEYS'];
 
 export const SnapshotCommand = ['SAVE', 'RESTORE'];
+
+export const allCommands = [
+  '• SET key value: set a string value, overwriting what is saved under key',
+  '• GET key: get a string value at key',
+  '• DEL key: Delete a key',
+  '• RPUSH key value1 [value2...]: append 1 or more values to the list, create a new list if not exists, return the length of the list after the operation',
+  '• RPOP key: remove and return the last item of the list',
+  '• LRANGE key start stop: return a range of elements from the list (zero-based, inclusive of start and stop), start and stop are non-negative integers',
+  '• SADD key value1 [value2...]: add values to the set stored at key',
+  '• SREM key value1 [value2...]: remove values from the set stored at key',
+  '• SMEMBERS key: return an array of all members of the set stored at key',
+  '• SINTER [key1] [key2] [key3] ...: build a set that is the intersection of all sets stored in specified keys. Return array of members of the result set',
+  '• KEYS: List all available keys',
+  '• DEL key: delete a key',
+  '• EXPIRE key seconds: set a timeout on a key, seconds is a positive integer (by default a key has no expiration). Return the number of seconds if the timeout is set',
+  '• TTL key: query the timeout of a key',
+  '• SAVE: save the current state in a snapshot',
+  '• RESTORE: restore the state from the last snapshot',
+  '• CLEAR: Clear the console',
+  '• TUTORIAL: View available commands and their functionality',
+];
--- src/pages/Homepage/index.tsx
+++ src/pages/Homepage/index.tsx
@@ -1,14 +1,15 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 import RedisIcon from '../../assets/svg/redis_icon.svg';
+import { allCommands } from '../../constants/command';
 import handleLedisCommand from '../../services/command.service';
 import dbService from '../../services/db.service';
 
 const Homepage = () => {
   const [inputCommand, setInputCommand] = useState<string>('');
-  const [commandList, setCommandList] = useState<string[]>(['>SET key value', 'OK']);
+  const [commandList, setCommandList] = useState<string[]>([]);
   const [isDBReady, setIsDBReady] = useState<boolean>(false);
-  console.log(isDBReady);
+  const consoleRef = useRef<HTMLDivElement>(null);
 
   const handleInitDB = async () => {
     const status = await dbService.initDB();
@@ -19,10 +20,20 @@ const Homepage = () => {
     handleInitDB();
   }, []);
 
+  useEffect(() => {
+    if (consoleRef.current) {
+      consoleRef.current?.lastElementChild?.scrollIntoView();
+    }
+  }, [commandList]);
+
   const handleCommand = async () => {
     if (inputCommand === '') return;
     if (inputCommand.toLowerCase() === 'clear') {
       setCommandList([]);
+      setInputCommand('');
+      return;
+    } else if (inputCommand.toLowerCase() === 'tutorial') {
+      setCommandList((prev) => [...prev, `>${inputCommand}`, ...allCommands]);
       setInputCommand('');
       return;
     }
@@ -41,11 +52,25 @@ const Homepage = () => {
           <p>Light weight Redis CLI</p>
         </div>
       </div>
-      <div className='mt-10 w-full px-5 md:px-10 lg:px-[80px] xl:px-0 xl:w-[50vw]'>
-        <div className='w-full bg-gray-200 h-[400px] rounded-t-lg p-2 flex flex-col overflow-y-auto custom-scrollbar'>
-          {commandList.map((command, index) => (
-            <p key={index}>{command}</p>
-          ))}
+      <div className='mt-10 w-full px-5 md:px-10 lg:px-[80px] xl:px-0 xl:w-[60vw]'>
+        <div className='w-full bg-[#D82C20] rounded-lg mb-5 p-2 flex flex-col space-y-2'>
+          <h2 className='text-white text-[18px]'>
+            Welcome to <b>Ledis</b>, a light weight Redis CLI
+          </h2>
+          <p className='text-white text-[18px]'>
+            Please type <b>TUTORIAL</b> view available commands and their functionality.
+          </p>
+        </div>
+        <div
+          ref={consoleRef}
+          className='w-full bg-gray-200 h-[480px] rounded-t-lg p-2 flex flex-col overflow-y-auto custom-scrollbar'
+        >
+          {!isDBReady && (
+            <div className='h-full w-full flex items-center justify-center'>
+              <p className='text-[18px]'>Loading...</p>
+            </div>
+          )}
+          {isDBReady && commandList.map((command, index) => <p key={index}>{command}</p>)}
         </div>
         <div className='w-full bg-black rounded-b-lg p-2 text-white flex space-x-1'>
           <p>{'>'}Ledis:</p>
